ec18192d5358f8411f8a03874bc9af8afb7b9c6e
Fernanda <119077188+Fernandadsantos@users.noreply.github.com>
2024-09-27 15:24:16 -0300

    Add files via upload

---
+++ projeto-poo/src/main/java/com/mycompany/projeto/poo/ProjetoPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto.poo;
+
+/**
+ *
+ * @author fernanda
+ */
+public class ProjetoPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
